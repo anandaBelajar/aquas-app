3d71b515a0ad72a4d687b9792c9bbeb4eccaf22d
Ananda Pamungkas <gedeananda@hotmail.com>
2020-11-07 00:41:44 +0800

    fix environmental variable error

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 var express = require('express'); //express package
 var mysql = require('mysql'); //mysql package
 
-
 var app = express(); //express function
 
 app.set('view engine', 'ejs'); //view engine
 app.set('views', __dirname + './../views');
 
-var port = 3000 //server port
+var port = process.env.SERVER_PORT //server port
 
-var server = app.listen(port, function() {
+var server = app.listen(process.env.SERVER_PORT, function() {
     //setup a server in port 3000
     console.log('listening to request on port ' + port);
     console.log('url:http://localhost:' + port);
@@ -19,21 +18,17 @@ var con = mysql.createConnection({
     //database config
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME
 });
 
 var sensor_controller = require('./../controllers/sensor-controller.js'); //sensor controller
-var websocket = require(__dirname + './../websocket.js'); //websocket
-
-console.log(__dirname)
+var websocket = require(__dirname + '/websocket.js'); //websocket
 
 //Static files
 app.use(express.static('./../public'));
 app.use('/chartjs', express.static(__dirname + './../node_modules/chart.js/dist/Chart.min.js')); //chartjs library
 app.use('/public', express.static(__dirname + './../public'));
-//app.use('/styles', express.static(__dirname + '/public/css')); //stylesheet
-//app.use('/scripts', express.static(__dirname + '/public/js')); //script 
 
 sensor_controller(app, con); //call sensorController
 websocket(server, con); //call websocket
