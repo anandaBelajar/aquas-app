e11d3b1bb62f5dd72d57bf807d17ffb8161f153c
Ananda Pamungkas <gedeananda@hotmail.com>
2020-11-06 23:23:48 +0800

    add dotenv functionality

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 var express = require('express'); //express package
 var mysql = require('mysql'); //mysql package
 
+
 var app = express(); //express function
 
 app.set('view engine', 'ejs'); //view engine
+app.set('views', __dirname + './../views');
 
 var port = 3000 //server port
 
@@ -15,20 +17,21 @@ var server = app.listen(port, function() {
 
 var con = mysql.createConnection({
     //database config
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'aquas'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-var sensor_controller = require('./controllers/sensor-controller.js'); //sensor controller
-var websocket = require(__dirname + '/websocket.js'); //websocket
+var sensor_controller = require('./../controllers/sensor-controller.js'); //sensor controller
+var websocket = require(__dirname + './../websocket.js'); //websocket
 
+console.log(__dirname)
 
 //Static files
-app.use(express.static('public'));
-app.use('/chartjs', express.static(__dirname + '/node_modules/chart.js/dist/Chart.min.js')); //chartjs library
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static('./../public'));
+app.use('/chartjs', express.static(__dirname + './../node_modules/chart.js/dist/Chart.min.js')); //chartjs library
+app.use('/public', express.static(__dirname + './../public'));
 //app.use('/styles', express.static(__dirname + '/public/css')); //stylesheet
 //app.use('/scripts', express.static(__dirname + '/public/js')); //script 
 
--- websocket.js
+++ websocket.js
@@ -7,9 +7,8 @@ let mqtt = require('mqtt'); //require mqtt package
 
 module.exports = function(server, con) { //exports the function
 
-    let client = mqtt.connect('mqtt://mqtt.eclipse.org:1883'); //connect to online broker
-    //let client = mqtt.connect('mqtt:broker.hivemq.com:1883'); //connect to online broker
-    //let client = mqtt.connect('mqtt:192.168.1.12:5000'); //connect to local broker
+    let client = mqtt.connect(process.env.MQTT_BROKER); //connect to online broker
+    //let client = mqtt.connect(process.env.LOCAL_BROKER); //connect to local broker
 
     var io = socket(server); //pass the server as socket parameter
 
@@ -47,7 +46,6 @@ module.exports = function(server, con) { //exports the function
         var dbDateTime = year + "-" + month + "-" + day + "- " + hour + ":" + minutes + ":" + seconds; //date time to save in database
 
         if (topic == 'aquas/feed') {
-            /*
             if (minutes % 60 == 0 && seconds == 0) {
                 //save the sensor value to database every 60 minutes
                 var sql = "INSERT INTO `light` (`value`, `date`) VALUES (";
@@ -57,7 +55,6 @@ module.exports = function(server, con) { //exports the function
                     if (err) throw err;
                 });
             }
-            */
             var feed = [message.toString(), time]; //save sensor value from mqtt message and current time 
             io.sockets.emit('aquas_feed_msg_arrive', feed); //send sensor value and current time to frontend websocket
         } else
